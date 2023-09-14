561396760e206971012a1c7e0b65e30bd777d8b9
Jon <jon.herman@ionic.io>
2023-09-14 15:51:53 -0400

    add sync and reload buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Capacitor Live Updates</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Capacitor Live Updates</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
