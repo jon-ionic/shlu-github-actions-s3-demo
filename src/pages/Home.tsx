91b84eed54897839d6f09cdcf039f513ee641969
Jon <jon.herman@ionic.io>
2024-06-26 12:17:37 -0400

    attempt portal build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,42 +7,14 @@ import {
   IonCardHeader,
   IonCardContent,
   IonCard,
-  IonSelect,
   IonCardTitle,
-  IonSelectOption,
   IonButton,
-  IonToast,
-  IonText,
 } from '@ionic/react';
-import { sync, reload, getConfig, setConfig, resetConfig, SyncResult } from '@capacitor/live-updates';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
-  const [syncResp, setSyncResp] = useState<SyncResult | null>(null);
-  const [channel, setChannel] = useState<string>('');
-  const [channelFromConfig, setChannelFromConfig] = useState<string>('');
-  const [toastOpen, setToastOpen] = useState<boolean>(false);
   const [count, setCount] = useState<number>(0);
-
-  useEffect(() => {
-    updateConfigState()
-  }, [channel])
-
-  const updateConfigState = async () => {
-    const config = await getConfig();
-    setChannelFromConfig(config?.channel || '')
-  }
-
-  const handleSync = async () => {
-    setSyncResp(await sync())
-  }
-
-  const handleChannelUpdate = async () => {
-    await setConfig({ channel });
-    updateConfigState();
-    setToastOpen(true)
-  }
 
   const VERSION = '0.0.1'
 
@@ -56,7 +28,7 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Capacitor Live Updates</IonTitle>
+            <IonTitle size="large">Capacitor Live Updates {VERSION}</IonTitle>
           </IonToolbar>
         </IonHeader>
         <div id='container'>
@@ -73,85 +45,7 @@ const Home: React.FC = () => {
               </IonButton>
             </IonCardContent>
           </IonCard>
-
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Select Channel ({channelFromConfig || 'not set'})</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonSelect 
-                label="Channel"
-                onIonChange={(e) => setChannel(e.detail.value)}
-                value={channel}
-              >
-                {
-                  [
-                    { label: 'Production', value: 'prod-0.0.1'},
-                    { label: 'Development', value: 'dev-0.0.1'},
-                  ].map((item: {label: string, value: string}) => (
-                    <IonSelectOption value={item.value}>
-                      {item.label}
-                    </IonSelectOption>)
-                  )
-                }
-              </IonSelect>
-              <IonButton
-                onClick={handleChannelUpdate}
-                style={{ display: 'flex'}}
-              >
-                Save Channel
-              </IonButton>
-              <IonButton
-                onClick={resetConfig}
-                color='danger'
-                style={{ display: 'flex'}}
-              >Reset Config</IonButton>
-            </IonCardContent>
-          </IonCard>
-
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Sync Live Update (current version {VERSION})</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              {(syncResp?.liveUpdate?.channel && syncResp?.snapshot?.buildId) && (
-                <IonText>
-                  Downloaded build id {syncResp?.snapshot?.buildId} from {syncResp?.liveUpdate?.channel} channel.
-                </IonText>
-              )}
-              {syncResp && (<pre style={{ overflowWrap: 'normal', textAlign: 'left', display: 'inline-block' }}>
-                {JSON.stringify(syncResp, null, 2)}
-              </pre>)}
-              <IonButton 
-                onClick={handleSync}
-                style={{ display: 'flex', justifyContent: 'center'}}
-              >
-                Sync
-              </IonButton>
-            </IonCardContent>
-          </IonCard>
-
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Reload App</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonButton 
-                onClick={async () => { reload(); setSyncResp(null) }} 
-                style={{ display: 'flex', justifyContent: 'center'}}
-              >
-                Reload
-              </IonButton>
-            </IonCardContent>
-          </IonCard>
         </div>
-      
-        <IonToast
-          isOpen={toastOpen}
-          message='Live update config set successfully.'
-          onDidDismiss={() => setToastOpen(false)}
-          duration={5000}
-        ></IonToast>
       </IonContent>
     </IonPage>
   );
