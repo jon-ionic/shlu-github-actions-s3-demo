1478fa860fa80ab0ed93f911281c1e2cdef74777
Jon <jon.herman@ionic.io>
2024-10-07 14:02:53 -0400

    last bit of ui stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,15 @@ import {
   IonText,
   IonInput,
 } from '@ionic/react';
-import { sync, reload, getConfig, setConfig, resetConfig, SyncResult, LiveUpdateConfig } from '@capacitor/live-updates';
+import { 
+  sync, 
+  reload, 
+  getConfig, 
+  setConfig, 
+  resetConfig, 
+  SyncResult, 
+  LiveUpdateConfig,
+} from '@capacitor/live-updates';
 import { AppInfo, App } from '@capacitor/app'
 import { Device, DeviceInfo } from '@capacitor/device';
 import { useState, useEffect } from 'react';
@@ -89,124 +97,126 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>App Info</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonText><h2><b>Bundle ID:</b> {appInfo?.id || 'Not set'}</h2></IonText>
-              <IonText><h2><b>Native version ID:</b> {appInfo?.build || 'Not set'}</h2></IonText>
-              <IonText><h2><b>Native version string:</b> {appInfo?.version || 'Not set'}</h2></IonText>
-              <IonText><h2><b>Web version:</b> {packageJsonVersion}</h2></IonText>
-            </IonCardContent>
-          </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>App Info</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonText><h2><b>Bundle ID:</b> {appInfo?.id || 'Not set'}</h2></IonText>
+            <IonText><h2><b>Native version ID:</b> {appInfo?.build || 'Not set'}</h2></IonText>
+            <IonText><h2><b>Native version string:</b> {appInfo?.version || 'Not set'}</h2></IonText>
+            <IonText><h2><b>Web version:</b> {packageJsonVersion}</h2></IonText>
+          </IonCardContent>
+        </IonCard>
 
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Device Info</IonCardTitle>
-            </IonCardHeader>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Device Info</IonCardTitle>
+          </IonCardHeader>
             <IonCardContent>
-              <IonText><h2><b>Platform:</b> {deviceInfo?.platform || 'Not set'}</h2></IonText>
-              {deviceInfo?.platform === 'ios' && (
-                <IonText><h2><b>iOS version:</b> {parseiOSVersion(deviceInfo?.iOSVersion) || 'Not set'}</h2></IonText>
-              )}
-              {deviceInfo?.platform === 'android' && (
-                <IonText><h2><b>Android SDK version:</b> {deviceInfo?.androidSDKVersion || 'Not set'}</h2></IonText>
-              )}
-              {['ios', 'android'].includes(deviceInfo?.platform || '') && (
-                <IonText><h2><b>Free space:</b> {bytesToHumanReadableSize(deviceInfo?.realDiskFree || 0) || 'Not set'}</h2></IonText>
-              )}
-            </IonCardContent>
-          </IonCard>
+            <IonText><h2><b>Platform:</b> {deviceInfo?.platform || 'Not set'}</h2></IonText>
+            {deviceInfo?.platform === 'ios' && (
+              <IonText><h2><b>iOS version:</b> {parseiOSVersion(deviceInfo?.iOSVersion) || 'Not set'}</h2></IonText>
+            )}
+            {deviceInfo?.platform === 'android' && (
+              <IonText><h2><b>Android SDK version:</b> {deviceInfo?.androidSDKVersion || 'Not set'}</h2></IonText>
+            )}
+            {['ios', 'android'].includes(deviceInfo?.platform || '') && (
+              <IonText><h2><b>Free space:</b> {bytesToHumanReadableSize(deviceInfo?.realDiskFree || 0) || 'Not set'}</h2></IonText>
+            )}
+          </IonCardContent>
+        </IonCard>
 
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Set Config</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonInput
-                label='App ID'
-                labelPlacement='stacked'
-                placeholder={liveUpdateConfig?.appId || 'Not set'}
-                value={appId}
-                onIonInput={(e) => setAppId(e.target?.value?.toString() || '')}
-              ></IonInput>
-              <IonInput
-                label='Channel'
-                labelPlacement='stacked'
-                placeholder={liveUpdateConfig?.channel || 'Not set'}
-                value={channel}
-                onIonInput={(e) => setChannel(e.target?.value?.toString() || '')}
-              />
-              <IonInput
-                label='Strategy'
-                labelPlacement='stacked'
-                placeholder={liveUpdateConfig?.strategy || 'Not set'}
-                value={strategy}
-                onIonInput={(e) => setStrategy(e.target?.value?.toString() || '')}
-              />
-              <IonButton
-                onClick={handleConfigUpdate}
-                style={{ display: 'flex'}}
-              >
-                Save Config
-              </IonButton>
-              <IonButton
-                onClick={handleConfigReset}
-                color='danger'
-                style={{ display: 'flex'}}
-              >Reset Config</IonButton>
-            </IonCardContent>
-          </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Set Config</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonInput
+              label='App ID'
+              labelPlacement='stacked'
+              placeholder={liveUpdateConfig?.appId || 'Not set'}
+              value={appId}
+              onIonInput={(e) => setAppId(e.target?.value?.toString() || '')}
+            ></IonInput>
+            <IonInput
+              label='Channel'
+              labelPlacement='stacked'
+              placeholder={liveUpdateConfig?.channel || 'Not set'}
+              value={channel}
+              onIonInput={(e) => setChannel(e.target?.value?.toString() || '')}
+            />
+            <IonInput
+              label='Strategy'
+              labelPlacement='stacked'
+              placeholder={liveUpdateConfig?.strategy || 'Not set'}
+              value={strategy}
+              onIonInput={(e) => setStrategy(e.target?.value?.toString() || '')}
+            />
+            <IonButton
+              onClick={handleConfigUpdate}
+              style={{ display: 'flex'}}
+            >
+              Save Config
+            </IonButton>
+            <IonButton
+              onClick={handleConfigReset}
+              color='danger'
+              style={{ display: 'flex'}}
+            >Reset Config</IonButton>
+          </IonCardContent>
+        </IonCard>
 
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Sync Live Update</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              {(downloadProgress > 0 && downloadProgress < 1) && (
-                <IonText>
-                  {Math.round(downloadProgress * 100)}% downloaded.
-                </IonText>
-              )}
-              {(syncResp?.liveUpdate?.channel && syncResp?.snapshot?.buildId) && (
-                <IonText>
-                  Downloaded build id {syncResp?.snapshot?.buildId} from {syncResp?.liveUpdate?.channel} channel.
-                </IonText>
-              )}
-              {syncResp && (<pre style={{ overflowWrap: 'normal', textAlign: 'left', display: 'inline-block' }}>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Sync Live Update</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            {(downloadProgress > 0 && downloadProgress < 1) && (
+              <IonText>
+                {Math.round(downloadProgress * 100)}% downloaded.
+              </IonText>
+            )}
+            {(syncResp?.liveUpdate?.channel && syncResp?.snapshot?.buildId) && (
+              <IonText>
+                Downloaded build id {syncResp?.snapshot?.buildId} from {syncResp?.liveUpdate?.channel} channel.
+              </IonText>
+            )}
+            {syncResp && (
+              <pre style={{ overflow: 'auto', textAlign: 'left', display: 'inline-block' }}>
                 {JSON.stringify(syncResp, null, 2)}
-              </pre>)}
-              <IonButton 
-                onClick={handleSync}
-                style={{ display: 'flex', justifyContent: 'center'}}
-              >
-                Sync
-              </IonButton>
-            </IonCardContent>
-          </IonCard>
+              </pre>
+            )}
+            <IonButton 
+              onClick={handleSync}
+              style={{ display: 'flex', justifyContent: 'center'}}
+            >
+              Sync
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
 
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Reload App</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonButton 
-                onClick={async () => { reload(); setSyncResp(null) }} 
-                style={{ display: 'flex', justifyContent: 'center'}}
-              >
-                Reload
-              </IonButton>
-            </IonCardContent>
-          </IonCard>
-        </IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Reload App</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonButton 
+              onClick={async () => { reload(); setSyncResp(null) }} 
+              style={{ display: 'flex', justifyContent: 'center'}}
+            >
+              Reload
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
       
-        <IonToast
-          isOpen={toastOpen}
-          message='Live update config set successfully.'
-          onDidDismiss={() => setToastOpen(false)}
-          duration={5000}
-        ></IonToast>
+      <IonToast
+        isOpen={toastOpen}
+        message='Live update config set successfully.'
+        onDidDismiss={() => setToastOpen(false)}
+        duration={5000}
+      ></IonToast>
     </IonPage>
   );
 };
