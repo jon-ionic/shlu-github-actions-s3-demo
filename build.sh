8c00e523f884532c24d0c6d8994f3bbc14156370
Jon Herman <jon.herman@outsystems.com>
2025-08-20 13:06:12 -0600

    maybe its the lingering ionic-cloud executable

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
-# echo "Installing Appflow CLI"
-# curl -fsSL https://ionic.io/get-appflow-cli | bash
+echo "Installing Appflow CLI"
+curl -fsSL https://ionic.io/get-appflow-cli | bash
 
-# echo "Running swad script"
-# python3 swad.py
-# echo "Script executed successfully."
+echo "Running swad script"
+python3 swad.py
+echo "Script executed successfully."
 
 echo "Running build script"
 npm run build
 
-# APPFLOW_BIN="$(which appflow)"
-# echo "Removing Appflow CLI at $APPFLOW_BIN"
-# rm -rf $APPFLOW_BIN
-# echo "Removed Appflow CLI"
+APPFLOW_BIN="$(which appflow)"
+IONIC_CLOUD_BIN="$(which ionic-cloud)
+echo "Removing Appflow CLI at $APPFLOW_BIN and $IONIC_CLOUD_BIN"
+rm -rf $APPFLOW_BIN
+rm -rf $IONIC_CLOUD_BIN
+echo "Removed Appflow CLI"
 
 echo "Build script successful".
