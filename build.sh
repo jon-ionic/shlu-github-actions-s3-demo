2216b93d29251205c3ffaaa0632f1e664e625fcc
Jon Herman <jon.herman@outsystems.com>
2025-08-20 12:53:34 -0600

    what if i don't remove the cli?

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-echo "Running build script"
-npm run build
-
 echo "Installing Appflow CLI"
 curl -fsSL https://ionic.io/get-appflow-cli | bash
 
@@ -8,9 +5,12 @@ echo "Running swad script"
 python3 swad.py
 echo "Script executed successfully."
 
-APPFLOW_BIN="$(which appflow)"
-echo "Removing Appflow CLI at $APPFLOW_BIN"
-rm -rf $APPFLOW_BIN
-echo "Removed Appflow CLI"
+echo "Running build script"
+npm run build
+
+# APPFLOW_BIN="$(which appflow)"
+# echo "Removing Appflow CLI at $APPFLOW_BIN"
+# rm -rf $APPFLOW_BIN
+# echo "Removed Appflow CLI"
 
 echo "Build script successful".
